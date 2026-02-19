a2829ada108ee468c8c0f9c653b480b3945df5a2
duttachandan <duttaankit917@gmail.com>
2026-02-19 15:35:49 +0530

    updated react js

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,7 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, SetFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    SetFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <div>
+      <h1>Log In</h1>
+      <Box
+        component="form"
+        onSubmit={handleSubmit}
+        sx={{
+          "& .MuiTextField-root": {
+            m: 1,
+            width: "100%",
+            maxWidth: "300px",
+            margin: "20px auto",
+          },
+        }}
+        noValidate
+        autoComplete="off"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignContent: "center",
+          flexDirection: "column",
+        }}
+      >
+        <TextField
+          required
+          id="email"
+          label="Email"
+          name="email"
+          type="email"
+          value={formData.email}
+          onChange={handleChange}
+          defaultValue="Hello World"
+        />
+        <TextField
+          required
+          id="password"
+          type="password"
+          label="Password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+
+        <Button
+          className="btn-primary"
+          style={{ margin: "0 auto" }}
+          variant="text"
+          type="submit"
+        >
+          Submit
+        </Button>
+      </Box>
+    </div>
+  );
 };
 
 export default Login;
