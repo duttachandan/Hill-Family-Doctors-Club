66a95c858bc9d67ce395d76e8c0a1a9ef1740470
duttachandan <duttaankit917@gmail.com>
2026-03-18 19:07:27 +0530

    updated modal page partially

--- Backend/app/routes/authRoutes.js
+++ Backend/app/routes/authRoutes.js
@@ -14,12 +14,15 @@ Router.post("/generateotp", wrapAsync(AuthController.generateOtp));
 // Admin Login
 Router.post("/adminlogin", wrapAsync(AuthController.adminLogin));
 
+
 // user and admin refreshToken
 Router.get("/refreshtoken", wrapAsync(AuthController.refreshToken));
 
 
 // Doctor Modules
 Router.get("/doctors", wrapAsync(DoctorController.getAllDoctors));
+
+
 
 // user only Routes
 Router.get(
@@ -33,6 +36,9 @@ Router.post(
   verifyAccessToken,
   wrapAsync(DoctorController.ReqAppointment),
 );
+
+
+
 
 // Admin Only Routes
 Router.post(
